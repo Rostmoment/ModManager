bcc39537226e8aa0f35a0779433bff65be8b79a8
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2024-09-14 23:57:22 +0300

    Add files via upload

---
+++ ModManager/BasePlugin.cs
@@ -0,0 +1,92 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.OptionsAPI;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace ModManager
+{
+    [BepInPlugin("rost.moment.baldiplus.modmanager", "Mods Manager", "2.0")]
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi", BepInDependency.DependencyFlags.HardDependency)]
+    public class BasePlugin : BaseUnityPlugin
+    {
+        public static BasePlugin Instance;
+        private void OnMenu(OptionsMenu menu)
+        {
+            GameObject category = CustomOptionsCore.CreateNewCategory(menu, "Mods\nManager");
+            ModManager.optionsMenu = menu;
+            ModManager.category = category;
+            ModManager modManager = category.AddComponent<ModManager>();
+            modManager.transform.SetParent(category.transform, false);
+        }
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("rost.moment.baldiplus.modmanager");
+            harmony.PatchAllConditionals();
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            CustomOptionsCore.OnMenuInitialize += OnMenu;
+        }
+        public static void UseCmd(string command)
+        {
+            UnityEngine.Debug.Log(command);
+            ProcessStartInfo processInfo = new ProcessStartInfo("cmd.exe", "/c " + command)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
+            };
+
+            Process process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                UnityEngine.Debug.Log("Output>>" + e.Data);
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                UnityEngine.Debug.Log("Error>>" + e.Data);
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            UnityEngine.Debug.Log($"ExitCode: {process.ExitCode}");
+            process.Close();
+        }
+        public static void RenameFiles(string fullPath, string newName) => UseCmd("rename \"" + fullPath + "\" \"" + newName + "\"");
+        public static T LoadAsset<T>(string name) where T : Object
+        {
+            return (from x in Resources.FindObjectsOfTypeAll<T>()
+                    where x.name.ToLower() == name.ToLower()
+                    select x).First();
+        }
+        public static StandardMenuButton CreateButtonWithSprite(string name, Sprite sprite, Sprite spriteOnHightlight = null, Transform parent = null, Vector3? positon = null)
+        {
+            GameObject gameObject = new GameObject(name);
+            gameObject.layer = 5;
+            gameObject.tag = "Button";
+            StandardMenuButton res = gameObject.AddComponent<StandardMenuButton>();
+            res.image = gameObject.AddComponent<Image>();
+            res.image.sprite = sprite;
+            res.unhighlightedSprite = sprite;
+            res.OnPress = new UnityEvent();
+            res.OnRelease = new UnityEvent();
+            if (spriteOnHightlight != null)
+            {
+                res.OnHighlight = new UnityEvent();
+                res.swapOnHigh = true;
+                res.highlightedSprite = spriteOnHightlight;
+            }
+            res.transform.SetParent(parent);
+            res.transform.localPosition = positon ?? new Vector3(0, 0, 0);
+            return res;
+        }
+    }
+}
---
+++ ModManager/ModManager.cs
@@ -0,0 +1,76 @@
+﻿using HarmonyLib;
+using MTM101BaldAPI.OptionsAPI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ModManager
+{
+    public class ModManager : MonoBehaviour
+    {
+        private int index = 0;
+        private StandardMenuButton previousButton;
+        private StandardMenuButton nextButton;
+        public static OptionsMenu optionsMenu;
+        public static GameObject category;
+        public TextLocalizer modName;
+        public TextLocalizer modInfo;
+        public List<ModInfo> mods = new List<ModInfo>();
+        private static bool CheckForHotKey(KeyCode keyCode) =>
+            (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl) || Input.GetKey(KeyCode.LeftCommand) || Input.GetKey(KeyCode.RightCommand)) && Input.GetKeyDown(keyCode);
+        void Start()
+        {
+            ModInfo.menu = optionsMenu;
+            ModInfo.category = category;
+            ModInfo.modManager = this;
+            foreach (string name in Directory.GetFiles(ModInfo.PluginsPath))
+            {
+                if (name.ToLower().EndsWith(ModInfo.dllEnabled) || name.ToLower().EndsWith(ModInfo.dllDisabled)) new ModInfo(Path.GetFileName(name));
+            }
+            CustomOptionsCore.CreateApplyButton(optionsMenu, "Apply changes", () => mods.Do(x => x.ChangeState())).transform.SetParent(category.transform, false);
+            previousButton = BasePlugin.CreateButtonWithSprite("PreviousButton", BasePlugin.LoadAsset<Sprite>("MenuArrowSheet_2"), BasePlugin.LoadAsset<Sprite>("MenuArrowSheet_0"), category.transform, new Vector3(-150, 30));
+            previousButton.OnPress = new UnityEngine.Events.UnityEvent();
+            previousButton.OnPress.AddListener(() => ChangeMod(false));
+            previousButton.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            nextButton = BasePlugin.CreateButtonWithSprite("NextButton", BasePlugin.LoadAsset<Sprite>("MenuArrowSheet_3"), BasePlugin.LoadAsset<Sprite>("MenuArrowSheet_1"), category.transform, new Vector3(150, 30));
+            nextButton.OnPress = new UnityEngine.Events.UnityEvent();
+            nextButton.OnPress.AddListener(() => ChangeMod(true));
+            nextButton.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+            modName = CustomOptionsCore.CreateText(optionsMenu, new Vector2(0, 30), "");
+            modName.transform.SetParent(category.transform, false);
+            modName.textBox.fontSize = 20;
+            modInfo = CustomOptionsCore.CreateText(optionsMenu, new Vector2(-20, -100), "");
+            modInfo.transform.SetParent(category.transform, false);
+            modInfo.textBox.fontSize = 20;
+            mods.Do(x => x.SetActive(false));
+            mods.First().SetActive(true);
+        }
+        void Update()
+        {
+            if (modName != null) modName.textBox.autoSizeTextContainer = true;
+            if (modInfo != null)
+            {
+                modInfo.textBox.autoSizeTextContainer = false;
+                modInfo.textBox.autoSizeTextContainer = true;
+            }
+            if (CheckForHotKey(KeyCode.C))
+            {
+
+            }
+        }
+        private void ChangeMod(bool state)
+        {
+            if (state) index++;
+            else index--;
+            if (index < 0) index = mods.Count - 1;
+            if (index >= mods.Count) index = 0;
+            for (int i = 0; i < mods.Count; i++)
+            {
+                mods[i].SetActive(i == index);
+            }
+        }
+    }
+}
